defd351a9a018fbd3af53218d83a22eff6a9fe3d
Bogdan Zanfir <56277980+Ragnarok54@users.noreply.github.com>
2020-03-31 15:45:25 +0300

    Added rotation, deletion, score and optimizations
    Optimizations include: better random, unordered_set and hash function for searching

--- GameData.h
+++ GameData.h
@@ -1,10 +1,23 @@
 #pragma once
+#include <random>
+#include <unordered_set>
+
+// Hash function for sorting Block type
+template <>
+struct hash<Block>
+{
+    size_t operator()(Block const& x) const noexcept
+    {
+        return ( (51 + std::hash<int>()(x.GetX())) * 51 + std::hash<int>()(x.GetY()) );
+    }
+};
 
 class GameData
 {
 private:
   int mBlocksHeight, mBlocksWidth;
   bool mAlive{ true };
+  int score = 0;
 
   void Init()
   {
@@ -67,9 +80,12 @@ public:
   void Add(Shape aShape)
   {
     // #STL homework rand() is a bad way to randomize, find a better modern C++ one
-    auto [r, g, b] = make_tuple(rand() % 100 / 100.0f,
-      rand() % 100 / 100.0f,
-      rand() % 100 / 100.0f);
+    random_device rd;
+    mt19937 mt(rd());
+    uniform_int_distribution<int> dist(0, 10000);
+    auto [r, g, b] = make_tuple(dist(mt) % 100 / 100.0f,
+                                dist(mt) % 100 / 100.0f,
+                                dist(mt) % 100 / 100.0f);
 
     const int startX = (int)(mBlocksWidth / 2) * Block::GetSize();
 
@@ -103,17 +119,17 @@ public:
     {
       mMovingBlocks.emplace_back(startX, 0, r, g, b);
       mMovingBlocks.emplace_back(startX, 1 * Block::GetSize(), r, g, b);
-      mMovingBlocks.emplace_back(startX + Block::GetSize(), -1 * Block::GetSize(), r, g, b);
-      mMovingBlocks.emplace_back(startX, -Block::GetSize(), r, g, b);
+      mMovingBlocks.emplace_back(startX , - Block::GetSize(), r, g, b);
+      mMovingBlocks.emplace_back(startX -Block::GetSize(), -Block::GetSize(), r, g, b);
       break;
     }
     case Shape::Z:
     {
       // #STL homework replace with emplace_back
-      mMovingBlocks.push_back(Block(startX, 0, r, g, b));
-      mMovingBlocks.push_back(Block(startX, -Block::GetSize(), r, g, b));
-      mMovingBlocks.push_back(Block(startX + Block::GetSize(), 0, r, g, b));
-      mMovingBlocks.push_back(Block(startX + Block::GetSize(), Block::GetSize(), r, g, b));
+      mMovingBlocks.emplace_back(Block(startX, 0, r, g, b));
+      mMovingBlocks.emplace_back(Block(startX, -Block::GetSize(), r, g, b));
+      mMovingBlocks.emplace_back(Block(startX + Block::GetSize(), 0, r, g, b));
+      mMovingBlocks.emplace_back(Block(startX + Block::GetSize(), Block::GetSize(), r, g, b));
 
       break;
     }
@@ -124,22 +140,36 @@ public:
   {
     // #STL we could use unordered_set
     // homework research & try how to write hash function for an unordered_set
+    
+    // Insert all the blocks into the unordered set
+    unordered_set<Block> walls(begin(mFrozenBlocks), end(mFrozenBlocks));
+    walls.insert(begin(mWallBlocks), end(mWallBlocks));
+    
+    Block *t = new Block(x, y);
 
+    if (walls.find(*t) != end(walls))
+        return true;
+    return false;
+    
+    /* Original function
+    for (auto val : walls) {
+        cout << val.GetX() << " " << val.GetY()<<endl;
+    }
     auto foundInWall = find_if(begin(mWallBlocks), end(mWallBlocks), [x, y](Block & block)
       {
         return block.GetX() == x && block.GetY() == y;
       });
     if (foundInWall != end(mWallBlocks))
       return true;
-
+    
 
     for (auto& block : mFrozenBlocks)
     {
       if (block.GetX() == x && block.GetY() == y)
         return true;
     }
-
     return false;
+    */
   }
 
   bool TryMove(int dx, int dy)
@@ -163,6 +193,30 @@ public:
     if (aKey == Key::Space)
     {
       // #STL homework rotate shape clockwise
+        bool CanRotate = true;
+        int CenterX = 0,CenterY = 0;
+
+        // Compute the origin point for Shape rotation
+        for (auto& block : mMovingBlocks) {
+            CenterX += block.GetX();
+            CenterY += block.GetY();
+        }
+        CenterX /= (mMovingBlocks.size() * Block::GetSize()) + 1 ;
+        CenterY /= (mMovingBlocks.size() * Block::GetSize());
+
+        // Check if rotation can be acomplished
+        for (auto& block : mMovingBlocks) {
+            if (HasObstacleAt(-block.GetY() + (CenterX + 1) * Block::GetSize() + CenterY * Block::GetSize(),
+                block.GetX() - CenterX * Block::GetSize() + CenterY * Block::GetSize()) == true)
+                CanRotate = false;
+        }
+        // Rotate shape if possible
+        if (CanRotate) {
+            for (auto& block : mMovingBlocks) {
+                block.SetPos(-block.GetY() + (CenterX + 1) * Block::GetSize() + CenterY * Block::GetSize(),
+                              block.GetX() - CenterX * Block::GetSize() + CenterY * Block::GetSize());
+            }
+        }
     }
     else if (aKey == Key::Left)
     {
@@ -203,16 +257,44 @@ public:
 
   void Update()
   {
-    if (!IsAlive())
-      return;
+      if (!IsAlive())
+          return;
 
-    // #STL homework we need to destroy blocks that line up on a X coordinate
+      // #STL homework we need to destroy blocks that line up on a X coordinate
+      vector<int> BlockAmount(mBlocksHeight + 1);
+    
+    // Compute the amount of blocks per row
+    for (auto block : mFrozenBlocks) {
+        BlockAmount[block.GetY() / Block::GetSize()]++;
+    }
+    
+    // Check if we have a line
+    for (int row = 0; row <= mBlocksHeight * Block::GetSize(); row += Block::GetSize()) {
+        if (BlockAmount[row / Block::GetSize()] == mBlocksWidth - 1) {
+            // Remove the line
+            mFrozenBlocks.erase(remove_if(begin(mFrozenBlocks), end(mFrozenBlocks),
+                                [row](Block block) {return row == block.GetY(); }),
+                                end(mFrozenBlocks));
+    
+            // Shift down the blocks above it
+            for (auto & block : mFrozenBlocks) {
+                if(block.GetY() <= row)
+                block.UpdatePos(0, 1);
+            }
+            // Get 10 points for clearing a line
+            score += 10;
+        }
+        BlockAmount[row / Block::GetSize()] = 0;
+    }
 
     if (!TryMove(0, 1))
       FreezeMovingBlocks();
 
-    if (mMovingBlocks.empty())
-      Add((Shape)(rand() % 5 + 1));
+    if (mMovingBlocks.empty()) {
+        Add((Shape)(rand() % 5 + 1));
+        // Get a point for every block
+        ++score;
+    }
   }
 
   void PrintGameOver()
@@ -221,6 +303,16 @@ public:
     txt->x = 127;
     txt->y = 340;
     S2D_DrawText(txt);
+  }
+
+  // Function to print score
+  void PrintScore()
+  {
+      S2D_Text* txt = S2D_CreateText("fonts\\HARNGTON.ttf", "", 60);
+      S2D_SetText(txt, "Score: %d", score);
+      txt->x = 227;
+      txt->y = 440;
+      S2D_DrawText(txt);
   }
 
   void Render()
@@ -237,8 +329,8 @@ public:
     else
     {
       PrintGameOver();
-
       // homework can you devise a scoring system?
+      PrintScore();
     }
   }
 
